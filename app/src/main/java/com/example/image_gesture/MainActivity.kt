666d574235588d9725227f6d3b812599ebe8ec5a
Zuhri <achmad.s.zuhri182@gmail.com>
2019-11-19 20:30:26 +0700

    image gesture

---
+++ app/src/main/java/com/example/image_gesture/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.image_gesture
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.widget.PopupMenu
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gesture_status.setOnLongClickListener(){
+            val popupMenu=PopupMenu(this,it)
+            popupMenu.setOnMenuItemClickListener { item->
+                when(item.itemId){
+                    R.id.menu_share->{
+                        val intent=Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.facebook.com"))
+                        startActivity(intent)
+                         true
+                    }
+
+                    R.id.menu_info->{
+                        Toast.makeText(this,"halo cuy", Toast.LENGTH_LONG).show()
+                         true
+                    }
+                    else->  false
+                }
+            }
+
+            popupMenu.inflate(R.menu.main)
+            try {
+                val fieldMPopup=PopupMenu::class.java.getDeclaredField("mPopup")
+                fieldMPopup.isAccessible=true
+                val mPopup=fieldMPopup.get(popupMenu)
+                mPopup.javaClass
+                    .getDeclaredMethod("setForceShowIcon",Boolean::class.java)
+                    .invoke(mPopup,true)
+            }catch (e:Exception){
+                Log.e("Main","error Show",e)
+            }
+            finally {
+                popupMenu.show()
+            }
+            true
+        }
+    }
+}
