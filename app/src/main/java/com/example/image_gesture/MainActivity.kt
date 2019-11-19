2aede0895605d703f9f37b08222f53082d7e8a07
Zuhri <achmad.s.zuhri182@gmail.com>
2019-11-20 00:08:48 +0700

    latihangest

--- app/src/main/java/com/example/image_gesture/MainActivity.kt
+++ app/src/main/java/com/example/image_gesture/MainActivity.kt
@@ -5,7 +5,6 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.GestureDetector
 import android.widget.PopupMenu
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -28,7 +27,7 @@ class MainActivity : AppCompatActivity(){
                     }
 
                     R.id.menu_info->{
-                        Toast.makeText(this,"Ada Toast gan!", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this,"Ada Toast!", Toast.LENGTH_LONG).show()
                          true
                     }
                     else->  false
@@ -36,20 +35,20 @@ class MainActivity : AppCompatActivity(){
             }
 
             popupMenu.inflate(R.menu.main_main)
-            try {
-                val fieldMPopup=PopupMenu::class.java.getDeclaredField("mPopup")
-                fieldMPopup.isAccessible=true
-                val mPopup=fieldMPopup.get(popupMenu)
-                mPopup.javaClass
-                    .getDeclaredMethod("setForceShowIcon",Boolean::class.java)
-                    .invoke(mPopup,true)
-            }catch (e:Exception){
-                Log.e("Main","error showing menu icon",e)
-            }
-            finally {
-                popupMenu.show()
-            }
-            true
+                try {
+                    val fieldMPopup=PopupMenu::class.java.getDeclaredField("mPopup")
+                    fieldMPopup.isAccessible=true
+                    val mPopup=fieldMPopup.get(popupMenu)
+                    mPopup.javaClass
+                        .getDeclaredMethod("setForceShowIcon",Boolean::class.java)
+                        .invoke(mPopup,true)
+                }catch (e:Exception){
+                    Log.e("Main","error showing menu icon",e)
+                }
+                finally {
+                    popupMenu.show()
+                }
+                true
         }
     }
 }
